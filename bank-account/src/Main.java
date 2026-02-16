b1769d36cc26372278d3fb4268b8d98bbd46f020
thomasbroussard <thomas@contactoffice.com>
2026-02-16 11:53:52 +0100

    Try with resources

--- bank-account/src/Main.java
+++ bank-account/src/Main.java
@@ -13,47 +13,47 @@ public class Main {
 
         System.out.println("Welcome to the bank !");
 
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            System.out.println("please enter the stock ticker :");
+
+            Stock stock = new Stock();
+            String tickerName = scanner.nextLine();
+            stock.setTicker(tickerName);
+            stock.setPrice(30000.0);
+
+            stock = new Stock("GOLD", 30000.0);
+
+            //create a scenario where a customer called Quentin wants to buy 1 stock of GOLD,
+            Customer customer = new Customer();
+            customer.setName("Quentin");
 
 
-        System.out.println("please enter the stock ticker :");
-
-        Stock stock = new Stock();
-        String tickerName = scanner.nextLine();
-        stock.setTicker(tickerName);
-        stock.setPrice(30000.0);
-
-        stock = new Stock("GOLD", 30000.0);
-
-        //create a scenario where a customer called Quentin wants to buy 1 stock of GOLD,
-        Customer customer = new Customer();
-        customer.setName("Quentin");
+            //create the required investment account with a sufficient balance
+            InvestmentAccount investmentAccount = new InvestmentAccount();
+            investmentAccount.setBalance(100000.0);
+            investmentAccount.setAccountNumber("123456789");
+            investmentAccount.setCustomer(customer);
 
 
-        //create the required investment account with a sufficient balance
-        InvestmentAccount investmentAccount = new InvestmentAccount();
-        investmentAccount.setBalance(100000.0);
-        investmentAccount.setAccountNumber("123456789");
-        investmentAccount.setCustomer(customer);
+            // when the operation is created, the balance is updated (initial minus the cost of the operation)
+
+            Double quantity = 1.0;
+            buyStockFromAccount(stock, quantity, investmentAccount);
+            // then Quentin decides to open a savings account and to initialize the balance to 3000€
+
+            SavingsAccount savingsAccount = new SavingsAccount();
+            savingsAccount.setBalance(3000.0);
 
 
-        // when the operation is created, the balance is updated (initial minus the cost of the operation)
+            // what will be the earned interest over 1 year if he doesn't change the balance
+            Double interestRate = DEFAULT_INTEREST_RATE;
+            savingsAccount.setInterestRate(interestRate);
+            savingsAccount.setBalance(savingsAccount.getBalance() + interestRate * savingsAccount.getBalance());
 
-        Double quantity = 1.0;
-        buyStockFromAccount(stock, quantity, investmentAccount);
-        // then Quentin decides to open a savings account and to initialize the balance to 3000€
+            System.out.println("final balances : \n - investment " + investmentAccount.getBalance() + "\n - savings " + savingsAccount.getBalance());
 
-        SavingsAccount savingsAccount = new SavingsAccount();
-        savingsAccount.setBalance(3000.0);
-
-
-        // what will be the earned interest over 1 year if he doesn't change the balance
-        Double interestRate = DEFAULT_INTEREST_RATE;
-        savingsAccount.setInterestRate(interestRate);
-        savingsAccount.setBalance(savingsAccount.getBalance() + interestRate * savingsAccount.getBalance());
-
-        System.out.println("final balances : \n - investment " + investmentAccount.getBalance() + "\n - savings " + savingsAccount.getBalance());
-
+        }
 
 
     }
